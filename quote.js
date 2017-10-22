383091bbc633fab631779673dfa2c8f2b5dddf72
Jose Tovar <tmjoseantonio@gmail.com>
2017-10-22 18:17:53 -0500

    #3 Improves Javascript

--- quote.js
+++ quote.js
@@ -1,28 +1,26 @@
+/**
+ * Fetch a quote from forismatic API
+ * 
+ * @return {void}
+ */
 function randomQuote() {
- $.ajax({
-      url: "https://api.forismatic.com/api/1.0/",
-      jsonp: "jsonp",
-      dataType: "jsonp",
-      data: {
-        method: "getQuote",
-        lang: "en",
-        format: "jsonp"
-      },
-      success: function(quote) {
-        $('#Quote').html('&ldquo;'+quote.quoteText+'&rdquo;')
-        $('#author').html("-"+quote.quoteAuthor)
-      }
-    });
+  $.ajax({
+    url: "https://api.forismatic.com/api/1.0/",
+    jsonp: "jsonp",
+    dataType: "jsonp",
+    data: {
+      method: "getQuote",
+      lang: "en",
+      format: "jsonp"
+    },
+    success: function(quote) {
+      $('#Quote').html('&ldquo;'+quote.quoteText+'&rdquo;');
+      $('#author').html("-"+quote.quoteAuthor);
+    }
+  });
 }
 
+randomQuote();
 
-
-//Click on the button to generate another random quote
-$(document).ready(function(){
-    randomQuote();
-$('#random').on('click', function() {
-  
-    randomQuote();
-   
-  });
-});
+// Click button event listener
+$('#random').on('click', randomQuote);
