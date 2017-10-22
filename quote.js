3144ea1c5c1bb7383cd79dc5e2d11ffcfb31b425
frameshft <adiletkadyrov@gmail.com>
2017-10-23 01:35:37 +0600

    Add twitter share link

--- quote.js
+++ quote.js
@@ -11,8 +11,37 @@ function randomQuote() {
       success: function(quote) {
         $('#Quote').html('&ldquo;'+quote.quoteText+'&rdquo;')
         $('#author').html("-"+quote.quoteAuthor)
+        tweetQuote(quote.quoteText, quote.quoteAuthor, "quotes, hacktoberfest")
       }
     });
+}
+
+function tweetQuote (quote, author, hashtags){
+  // Build a twitterMessage
+  var twitterMessage = '"' + quote + '" - ' + author;
+
+  var elem = document.getElementById('twitterbutton');
+
+  // Remove the twitter share button, if it already exists
+  if (elem !== null) {
+      elem.parentNode.removeChild(elem);
+  }
+
+  // Build a new twitter link
+  var link = document.createElement('a');
+  link.setAttribute('href', 'https://twitter.com/intent/tweet');
+  link.setAttribute('class', 'twitter-share-button');
+  link.setAttribute('id', 'tweet');
+  link.setAttribute('data-text', twitterMessage);
+  link.setAttribute('data-size', 'large');
+  link.setAttribute('data-hashtags', hashtags);
+
+  // Append the new twitter link to the target
+  tweetDiv = document.getElementById('tweet')
+  tweetDiv.appendChild(link);
+
+  // Load it
+  twttr.widgets.load();
 }
 
 
@@ -21,8 +50,8 @@ function randomQuote() {
 $(document).ready(function(){
     randomQuote();
 $('#random').on('click', function() {
-  
+
     randomQuote();
-   
+
   });
 });
