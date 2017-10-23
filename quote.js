beffe67246ad15ba6e02d798ad6137a2c1bdc6d6
Sunil K. Jain <rpspjjjj@gmail.com>
2017-10-23 14:04:30 +0530

    Revert "Adding Twitter Button"

--- quote.js
+++ quote.js
@@ -1,29 +1,28 @@
 function randomQuote() {
-    $.ajax({
-        url: "https://api.forismatic.com/api/1.0/",
-        jsonp: "jsonp",
-        dataType: "jsonp",
-        data: {
-            method: "getQuote",
-            lang: "en",
-            format: "jsonp"
-        },
-        success: function (quote) {
-            $('#Quote').html('&ldquo;' + quote.quoteText + '&rdquo;')
-            $('#author').html("-" + quote.quoteAuthor);
-            $('#twitterText').attr('href', 'https://twitter.com/share?text=' + quote.quoteText + "  -" + quote.quoteAuthor);
-        }
+ $.ajax({
+      url: "https://api.forismatic.com/api/1.0/",
+      jsonp: "jsonp",
+      dataType: "jsonp",
+      data: {
+        method: "getQuote",
+        lang: "en",
+        format: "jsonp"
+      },
+      success: function(quote) {
+        $('#Quote').html('&ldquo;'+quote.quoteText+'&rdquo;')
+        $('#author').html("-"+quote.quoteAuthor)
+      }
     });
 }
 
 
 
 //Click on the button to generate another random quote
-$(document).ready(function () {
+$(document).ready(function(){
     randomQuote();
-    $('#random').on('click', function () {
-
-        randomQuote();
-
-    });
+$('#random').on('click', function() {
+  
+    randomQuote();
+   
+  });
 });
